ade7ddd4c07f0c1a784b83e4798f3545a5a5bf01
walze <wallacefares@gmail.com>
2022-09-12 14:07:46 -0300

    refactor: observables

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,47 @@
 <script lang="ts">
-  import { animationFrames } from 'rxjs'
+  import {
+    animationFrames,
+    Observable,
+    Subscription,
+  } from 'rxjs'
 
-  import { onMount } from 'svelte'
+  import { onDestroy, onMount } from 'svelte'
   import { stage } from './canvas/particles'
   import { randomInt } from './helpers/random'
   import { renderer } from './canvas/render'
-
-  animationFrames().subscribe(() => {
-    for (const p of stage.children) {
-      p.setTransform(
-        p.x + randomInt(-1, 1),
-        p.y + randomInt(-1, 1),
-      )
-    }
-  })
+  import { writable } from 'svelte/store'
 
   let view: HTMLCanvasElement
 
-  onMount(() => {
-    const { render } = renderer(view)
+  const useObservable = <T, _>(o: Observable<T>) => {
+    let sub: Subscription
+    const store = writable<T | null>(null)
 
-    animationFrames().subscribe(() => {
+    onMount(() => (sub = o.subscribe(store.set)))
+
+    onDestroy(() => sub.unsubscribe())
+
+    return store
+  }
+
+  const frames = useObservable(animationFrames())
+
+  $: {
+    if ($frames)
+      for (const p of stage.children)
+        p.setTransform(
+          p.x + randomInt(-1, 1),
+          p.y + randomInt(-1, 1),
+        )
+  }
+
+  $: {
+    if ($frames) {
+      const { render } = renderer(view)
+
       render(stage)
-    })
-  })
+    }
+  }
 </script>
 
 <canvas bind:this={view} />
